8ec4f91e0e53c0ae445e4958010eaa191cd80bd2
Dave Glowacki <dglo@icecube.wisc.edu>
2007-11-27 16:58:54 +0000

    Add setVerbose() method which prints log msgs to console for debugging

--- src/test/java/icecube/daq/io/test/LoggingCase.java
+++ src/test/java/icecube/daq/io/test/LoggingCase.java
@@ -46,6 +46,11 @@ public class LoggingCase
         BasicConfigurator.configure(appender);
     }
 
+    public void setVerbose(boolean val)
+    {
+        appender.setVerbose(val);
+    }
+
     protected void tearDown()
         throws Exception
     {
