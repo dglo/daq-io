23d4e953b5be26e023a0f25675235eb69a6dd9bd
Dave Glowacki <dglo@icecube.wisc.edu>
2009-09-15 19:15:00 +0000

    Dump multiple files and if -h is specified, dump payload bytes before printing debugging string

--- src/main/java/icecube/daq/io/PayloadDumper.java
+++ src/main/java/icecube/daq/io/PayloadDumper.java
@@ -1,8 +1,10 @@
 package icecube.daq.io;
 
+import icecube.daq.payload.impl.BasePayload;
 import icecube.daq.payload.ILoadablePayload;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -17,19 +19,40 @@ public class PayloadDumper
     {
         BasicConfigurator.configure();
 
-        DAQFileReader rdr = new PayloadFileReader(args[0]);
-        for (Object obj : rdr) {
-            ILoadablePayload payload = (ILoadablePayload) obj;
+        boolean dumpHex = false;
 
-            try {
-                payload.loadPayload();
-            } catch (Exception ex) {
-                LOG.error("Couldn't load payload");
-                ex.printStackTrace();
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].startsWith("-")) {
+                if (args[i].charAt(1) == 'h') {
+                    dumpHex = true;
+                } else {
+                    throw new Error("Unknown option \"" + args[i] + "\"");
+                }
+
                 continue;
             }
 
-            System.out.println(payload.toString());
+            DAQFileReader rdr = new PayloadFileReader(args[i]);
+            for (Object obj : rdr) {
+                ILoadablePayload payload = (ILoadablePayload) obj;
+
+                if (dumpHex) {
+                    ByteBuffer buf = payload.getPayloadBacking();
+                    if (buf != null) {
+                        System.err.println(BasePayload.toHexString(buf, 0));
+                    }
+                }
+
+                try {
+                    payload.loadPayload();
+                } catch (Exception ex) {
+                    LOG.error("Couldn't load payload");
+                    ex.printStackTrace();
+                    continue;
+                }
+
+                System.out.println(payload.toString());
+            }
         }
     }
 }
