b246aeea56ae11c108a4dc68eb0609a4c46bd2a7
Dave Glowacki <dglo@icecube.wisc.edu>
2011-02-08 16:31:57 +0000

    Add convenience methods

--- src/main/java/icecube/daq/io/BufferWriter.java
+++ src/main/java/icecube/daq/io/BufferWriter.java
@@ -69,6 +69,33 @@ public class BufferWriter
     }
 
     /**
+     * Open a uniquely named file for writing.  If the specified name
+     * is unavailable, a sequentially increasing number is appended
+     * until a unique file name is found.
+     *
+     * @param baseName base filename
+     *
+     * @return handle to be used for writing buffers
+     */
+    public static final BufferWriter openNext(String baseName)
+        throws IOException
+    {
+        String fileName = baseName;
+        int num = 0;
+
+        File file = null;
+        while (true) {
+            file = new File(fileName);
+            if (!file.exists()) {
+                break;
+            }
+            fileName = baseName + "." + num++;
+        }
+
+        return new BufferWriter(file, false);
+    }
+
+    /**
      * Write the buffer to the file.
      *
      * @throws IOException if there was a problem writing the buffer
@@ -77,5 +104,44 @@ public class BufferWriter
         throws IOException
     {
         return chan.write(buf);
+    }
+
+    /**
+     * Write the buffer to the file, preserving the current buffer position.
+     *
+     * @param buf buffer to be written
+     *
+     * @throws IOException if there was a problem writing the buffer
+     */
+    public int writeAndRestore(ByteBuffer buf)
+        throws IOException
+    {
+        return writeAndRestore(buf, 0);
+    }
+
+    /**
+     * Write the buffer to the file, preserving the current buffer position.
+     *
+     * @param buf buffer to be written
+     * @param offset buffer offset
+     *
+     * @throws IOException if there was a problem writing the buffer
+     */
+    public int writeAndRestore(ByteBuffer buf, int offset)
+        throws IOException
+    {
+        if (buf == null) {
+            throw new IOException("Cannot write null buffer");
+        }
+
+        final int pos = buf.position();
+        buf.position(offset);
+        int result;
+        try {
+            result = write(buf);
+        } finally {
+            buf.position(pos);
+        }
+        return result;
     }
 }
