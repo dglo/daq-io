c65f6ebce2c497ec0104960c70a61116a43dbff0
Tim Bendfelt <tim.bendfelt@icecube.wisc.edu>
2016-11-03 10:27:05 -0500

    Issue 7865: Add support for blocking output implementations.

--- src/main/java/icecube/daq/io/DAQComponentOutputProcess.java
+++ src/main/java/icecube/daq/io/DAQComponentOutputProcess.java
@@ -1,7 +1,7 @@
 /*
  * class: DAQComponentOutputProcess
  *
- * Version $Id: DAQComponentOutputProcess.java 15428 2015-02-20 19:01:46Z dglo $
+ * Version $Id: DAQComponentOutputProcess.java 16312 2016-11-03 20:27:05Z bendfelt $
  *
  * Date: March 24 2005
  *
@@ -18,12 +18,31 @@ import java.nio.channels.WritableByteChannel;
 /**
  * This represents the engine for the transmit channels.
  *
- * @version $Id: DAQComponentOutputProcess.java 15428 2015-02-20 19:01:46Z dglo $
+ * @version $Id: DAQComponentOutputProcess.java 16312 2016-11-03 20:27:05Z bendfelt $
  * @author mcp
  */
 public interface DAQComponentOutputProcess
     extends DAQComponentIOProcess, DAQOutputChannelManager
 {
+
+    /**
+     * Enumerate available channel options.
+     */
+    public enum ChannelRequirements
+    {
+        BLOCKING,
+        NON_BLOCKING
+    }
+
+    /**
+     * Advertises the required configuration of the channel
+     * provided to the connect() and addDataChannel() methods.
+     * By default, a non-blocking channel will be requested.
+     */
+    default public ChannelRequirements getChannelRequirement()
+    {
+        return ChannelRequirements.NON_BLOCKING;
+    }
 
     QueuedOutputChannel addDataChannel(WritableByteChannel channel,
                                        IByteBufferCache bufMgr);
@@ -31,6 +50,13 @@ public interface DAQComponentOutputProcess
     QueuedOutputChannel connect(IByteBufferCache bufMgr,
                                 WritableByteChannel chan, int srcId)
         throws IOException;
+
+    public default OutputChannel connect(final IByteBufferCache bufMgr,
+                                 String host, int port, String type)
+            throws IOException
+    {
+        throw new Error("not implemented");
+    }
 
     void disconnect()
         throws IOException;
