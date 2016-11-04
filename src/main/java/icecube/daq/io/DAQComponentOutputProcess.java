9b8904b2e85f44e228a20d1e06c5eb3baf782cf7
Tim Bendfelt <bendfelt@icecube.wisc.edu>
2016-11-04 18:27:16 +0000

    Removed non-standard connect() method added in last revision.

--- src/main/java/icecube/daq/io/DAQComponentOutputProcess.java
+++ src/main/java/icecube/daq/io/DAQComponentOutputProcess.java
@@ -1,7 +1,7 @@
 /*
  * class: DAQComponentOutputProcess
  *
- * Version $Id: DAQComponentOutputProcess.java 16312 2016-11-03 20:27:05Z bendfelt $
+ * Version $Id: DAQComponentOutputProcess.java 16323 2016-11-04 18:27:16Z bendfelt $
  *
  * Date: March 24 2005
  *
@@ -18,7 +18,7 @@ import java.nio.channels.WritableByteChannel;
 /**
  * This represents the engine for the transmit channels.
  *
- * @version $Id: DAQComponentOutputProcess.java 16312 2016-11-03 20:27:05Z bendfelt $
+ * @version $Id: DAQComponentOutputProcess.java 16323 2016-11-04 18:27:16Z bendfelt $
  * @author mcp
  */
 public interface DAQComponentOutputProcess
@@ -50,13 +50,6 @@ public interface DAQComponentOutputProcess
     QueuedOutputChannel connect(IByteBufferCache bufMgr,
                                 WritableByteChannel chan, int srcId)
         throws IOException;
-
-    public default OutputChannel connect(final IByteBufferCache bufMgr,
-                                 String host, int port, String type)
-            throws IOException
-    {
-        throw new Error("not implemented");
-    }
 
     void disconnect()
         throws IOException;
