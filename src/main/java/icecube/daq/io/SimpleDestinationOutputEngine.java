38dfb2d75ef05cd9eaaebf61a95289a6137e457f
Dave Glowacki <dglo@icecube.wisc.edu>
2018-10-04 11:14:35 -0500

    Issue 8501: ByteBufferPayloadDestination is now an internal class of SimpleDestinationOutputEngine

--- src/main/java/icecube/daq/io/SimpleDestinationOutputEngine.java
+++ src/main/java/icecube/daq/io/SimpleDestinationOutputEngine.java
@@ -24,149 +24,6 @@ import java.nio.ByteBuffer;
 import java.nio.channels.WritableByteChannel;
 import java.util.HashMap;
 
-/**
- * This object is a PayloadDestination that is able to write a Payload to
- * a new ByteBuffer which is acquired from an IByteBufferCache and pass this
- * newly created and written-to ByteBuffer (which now contains the contents of
- * a Payload) to a QueuedOutputChannel.
- *
- * @author dwharton
- */
-class ByteBufferPayloadDestination
-    extends PayloadDestination
-{
-    /**
-     * The IByteBufferCache from which new ByteBuffer's are acquired to
-     * write Payloads to.
-     */
-    private IByteBufferCache cache;
-
-    /**
-     * The QueuedOutputChannel object which is to receive the ByteBuffer that
-     * has been allocated and written into.
-     */
-    protected QueuedOutputChannel outChan;
-
-    /**
-     * Constructor.
-     * @param receiver QueuedOutputChannel the object which will receive the
-     *  ByteBuffer which has been created by subsequent calls to the
-     *  PayloadDestination.
-     * @param cache the IByteBufferCache which is used to acquire byte-buffers
-     *  to write to.
-     */
-    public ByteBufferPayloadDestination(QueuedOutputChannel receiver,
-                                        IByteBufferCache cache) {
-        if (cache == null) {
-            throw new Error("Buffer cache is null");
-        }
-
-        outChan = receiver;
-        this.cache = cache;
-    }
-
-    /**
-     * This methods proxies the call to write Payload to allow the whole
-     * payload to be passed to the payload destination to allow it to
-     * be invoke the write method itself, or to pass the payload by refernce
-     * to the target.
-     *
-     * @param tPayload Payload to which to write to this destination
-     * @return the length in bytes which was written to the ByteBuffer.
-     *
-     * @throws IOException if an error occurs during the process
-     */
-    @Override
-    public int writePayload(IWriteablePayload tPayload) throws IOException {
-        return writePayload(false,tPayload);
-    }
-
-    /**
-     * This methods proxies the call to write Payload to allow the whole
-     * payload to be passed to the payload destination to allow it to
-     * be invoke the write method itself, or to pass the payload by refernce
-     * to the target.
-     *
-     * @param bWriteLoaded boolean to indicate if the loaded vs buffered
-     *  payload should be written.
-     * @param tPayload Payload to which to write to this destination
-     * @return the length in bytes which was written to the ByteBuffer.
-     *
-     * @throws IOException if an error occurs during the process
-     */
-    @Override
-    public int writePayload(boolean bWriteLoaded, IWriteablePayload tPayload)
-        throws IOException
-    {
-        if (outChan == null) {
-            throw new IOException("This PayloadDestination is not valid");
-        }
-
-        int iPayloadLength = tPayload.length();
-        ByteBuffer tBuffer = cache.acquireBuffer(iPayloadLength);
-        if (tBuffer == null) {
-            throw new RuntimeException("Could not acquire buffer");
-        }
-
-        tBuffer.clear();
-        int iWrittenLength = tPayload.writePayload(bWriteLoaded,0,tBuffer);
-        if (iPayloadLength != iWrittenLength) {
-            throw new RuntimeException("Problem when acquireBuffer" +
-                                       " iPayloadLength: " + iPayloadLength +
-                                       " iWrittenLength: " + iWrittenLength +
-                                       " tBuffer: " + tBuffer.capacity());
-        }
-        //-this makes sure that the buffer position, capacity, etc is set.
-        tBuffer.clear();
-        tBuffer.position(0);
-        tBuffer.limit(iWrittenLength);
-        //
-        notifyReceiver(tBuffer);
-        return iWrittenLength;
-
-    }
-
-    /**
-     * Notifies the installed receiver of the new byte-buffer which has been
-     * created.
-     * @param tBuffer the new ByteBuffer which has been created.
-     */
-    public void notifyReceiver(ByteBuffer tBuffer) {
-        outChan.receiveByteBuffer(tBuffer);
-    }
-
-    /**
-     * Optionally receive the ByteBuffer back for reuse.
-     * @param  tBuffer ByteBuffer the buffer which can be reused.
-     */
-    public void recycleByteBuffer(ByteBuffer tBuffer) {
-        cache.returnBuffer(tBuffer);
-    }
-
-    /**
-     * Closes this channel.
-     *
-     * <p> After a channel is closed, any further attempt to invoke I/O
-     * operations upon it will cause a
-     * {@link java.nio.channels.ClosedChannelException} to be thrown.
-     *
-     * <p> If this channel is already closed then invoking this method has no
-     * effect.
-     *
-     * <p> This method may be invoked at any time.  If some other thread has
-     * already invoked it, however, then another invocation will block until
-     * the first invocation is complete, after which it will return without
-     * effect. </p>
-     *
-     * @throws  IOException  If an I/O error occurs
-     */
-    @Override
-    public void close() throws IOException {
-        outChan.flushOutQueue();
-        outChan = null;
-    }
-}
-
 public class SimpleDestinationOutputEngine
     extends SimpleOutputEngine
     implements DAQSourceIdOutputProcess,
@@ -322,5 +179,151 @@ public class SimpleDestinationOutputEngine
         QueuedOutputChannel eng = (QueuedOutputChannel) idRegistry.get(id);
         eng.receiveByteBuffer(payload);
         messagesSent++;
+    }
+
+    /**
+     * This object is a PayloadDestination that is able to write a Payload to
+     * a new ByteBuffer which is acquired from an IByteBufferCache and pass
+     * this newly created and written-to ByteBuffer (which now contains the
+     * contents of a Payload) to a QueuedOutputChannel.
+     *
+     * @author dwharton
+     */
+    class ByteBufferPayloadDestination
+        extends PayloadDestination
+    {
+        /**
+         * The IByteBufferCache from which new ByteBuffer's are acquired to
+         * write Payloads to.
+         */
+        private IByteBufferCache cache;
+
+        /**
+         * The QueuedOutputChannel object which is to receive the ByteBuffer
+         * that has been allocated and written into.
+         */
+        protected QueuedOutputChannel outChan;
+
+        /**
+         * Constructor.
+         * @param receiver QueuedOutputChannel the object which will receive
+         *  the ByteBuffer which has been created by subsequent calls to the
+         *  PayloadDestination.
+         * @param cache the IByteBufferCache which is used to acquire
+         *  byte-buffers to write to.
+         */
+        public ByteBufferPayloadDestination(QueuedOutputChannel receiver,
+                                            IByteBufferCache cache) {
+            if (cache == null) {
+                throw new Error("Buffer cache is null");
+            }
+
+            outChan = receiver;
+            this.cache = cache;
+        }
+
+        /**
+         * This methods proxies the call to write Payload to allow the whole
+         * payload to be passed to the payload destination to allow it to
+         * be invoke the write method itself, or to pass the payload by
+         * refernce to the target.
+         *
+         * @param tPayload Payload to which to write to this destination
+         * @return the length in bytes which was written to the ByteBuffer.
+         *
+         * @throws IOException if an error occurs during the process
+         */
+        @Override
+        public int writePayload(IWriteablePayload tPayload)
+            throws IOException
+        {
+            return writePayload(false,tPayload);
+        }
+
+        /**
+         * This methods proxies the call to write Payload to allow the whole
+         * payload to be passed to the payload destination to allow it to
+         * be invoke the write method itself, or to pass the payload by
+         * refernce to the target.
+         *
+         * @param bWriteLoaded boolean to indicate if the loaded vs buffered
+         *  payload should be written.
+         * @param tPayload Payload to which to write to this destination
+         * @return the length in bytes which was written to the ByteBuffer.
+         *
+         * @throws IOException if an error occurs during the process
+         */
+        @Override
+        public int writePayload(boolean bWriteLoaded,
+                                IWriteablePayload tPayload)
+            throws IOException
+        {
+            if (outChan == null) {
+                throw new IOException("This PayloadDestination is not valid");
+            }
+
+            int iPayloadLength = tPayload.length();
+            ByteBuffer tBuffer = cache.acquireBuffer(iPayloadLength);
+            if (tBuffer == null) {
+                throw new RuntimeException("Could not acquire buffer");
+            }
+
+            tBuffer.clear();
+            int iWrittenLength = tPayload.writePayload(bWriteLoaded,0,tBuffer);
+            if (iPayloadLength != iWrittenLength) {
+                throw new RuntimeException("Problem when acquireBuffer" +
+                                           " iPayloadLength: " + iPayloadLength +
+                                           " iWrittenLength: " + iWrittenLength +
+                                           " tBuffer: " + tBuffer.capacity());
+            }
+            //-this makes sure that the buffer position, capacity, etc is set.
+            tBuffer.clear();
+            tBuffer.position(0);
+            tBuffer.limit(iWrittenLength);
+            //
+            notifyReceiver(tBuffer);
+            return iWrittenLength;
+
+        }
+
+        /**
+         * Notifies the installed receiver of the new byte-buffer which has
+         * been created.
+         * @param tBuffer the new ByteBuffer which has been created.
+         */
+        public void notifyReceiver(ByteBuffer tBuffer) {
+            outChan.receiveByteBuffer(tBuffer);
+        }
+
+        /**
+         * Optionally receive the ByteBuffer back for reuse.
+         * @param  tBuffer ByteBuffer the buffer which can be reused.
+         */
+        public void recycleByteBuffer(ByteBuffer tBuffer) {
+            cache.returnBuffer(tBuffer);
+        }
+
+        /**
+         * Closes this channel.
+         *
+         * <p> After a channel is closed, any further attempt to invoke I/O
+         * operations upon it will cause a
+         * {@link java.nio.channels.ClosedChannelException} to be thrown.
+         *
+         * <p> If this channel is already closed then invoking this method has
+         * no effect.
+         *
+         * <p> This method may be invoked at any time.  If some other thread
+         * has already invoked it, however, then another invocation will block
+         * until the first invocation is complete, after which it will return
+         * without effect. </p>
+         *
+         * @throws  IOException  If an I/O error occurs
+         */
+        @Override
+        public void close() throws IOException {
+            outChan.flushOutQueue();
+            outChan = null;
+        }
     }
 }
